e2d871c5bd86aa0a52111e9e87ee16709f755dc9
forlorn <clickshaq11@gmail.com>
2024-04-13 23:04:02 +0300

    add image failsafe url

--- src/pages/Movie/components/Actors/MiniActor/index.tsx
+++ src/pages/Movie/components/Actors/MiniActor/index.tsx
@@ -1,17 +1,17 @@
 import { Actor } from '@/types/search';
 import styles from './styles.module.css';
 
+const NO_ACTOR_PHOTO_URL = 'https://www.kinopoisk.ru/images/no-poster.gif';
+
 function MiniActor({ name, photo, enName }: Actor) {
   return (
     <section className={styles.box}>
       <span className={styles.cn_name}>{name || enName}</span>
-      {photo && (
-        <img
-          src={photo}
-          alt={`Фотография ${name}`}
-          className={styles.cn_photo}
-        />
-      )}
+      <img
+        src={photo || NO_ACTOR_PHOTO_URL}
+        alt={`Фотография ${name}`}
+        className={styles.cn_photo}
+      />
     </section>
   );
 }
